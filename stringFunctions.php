32ef626debff79158dd38e0ad8ccd8db7abd36a2
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2023-11-20 13:07:39 +0100

    stringFunctions

---
+++ stringFunctions.php
@@ -0,0 +1,38 @@
+<?php
+
+$username = "Luca Iannice";
+$phone = "123-456-789";
+$userArray = array("Luca", "The Magic", "Iannice");
+
+// $username = strtolower($username); all lowercase
+// $username = strtoupper($username); all uppercase
+// $username = trim($username); no spaces
+// $username = str_pad($username, 20, "0"); add extra spaces (20 total spaces including) and fill it with "0" 
+$phone = str_replace("-", "/", $phone); // replace something with something else
+// $username = strrev($username);
+// $username = str_shuffle($username);
+$equals = strcmp($username, "Luca Iannice"); //compare and returns true or false
+$count = strlen($phone); // length of the string
+$index = strpos($phone, "/"); // indicates the position of a specific character
+$firstname = substr($username, 0, 4); // takes username and estrapolate from index 0 included to 4 excluded
+$lastname = substr($username, 5); // take everything after index 5 included
+$fullname = explode(" ", $username); // creates an array with each single part of the string divided by " "
+$userArray = implode("-", $userArray); // transform the array into a string. Each element is separated by "-"
+
+
+
+echo $username . "<br>";
+echo $phone . "<br>";
+echo $equals . "<br>";
+echo $count . "<br>";
+echo $index . "<br>";
+echo $firstname . "<br>";
+echo $lastname . "<br>";
+foreach ($fullname as $name) {
+    echo $name . "<br>";
+}
+echo $userArray . "<br>";
+
+
+
+?>
