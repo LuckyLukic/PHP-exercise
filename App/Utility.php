a5c0a13c20cd7dceac335ed4f440db493dacdccb
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2024-01-18 22:14:27 +0100

    throwing exception

--- App/Utility.php
+++ App/Utility.php
@@ -19,6 +19,11 @@ class Utility
     //static method, non dobbiamo istanziare una classe per accederci. classe utility solitamente e' fatta da static method per metodi di servizio generale
     public static function printArr(array $array)
     {
+
+        if (count($array) === 0) {
+            //throw new \Exception("Array is empty"); //generic
+            throw new \InvalidArgumentException("Array is empty");  //specific
+        }
         echo "<pre>";
         print_r($array);
         echo "</pre>";
