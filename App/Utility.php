3539cbf9ac75039a17691c6d023540c38f51480a
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2024-01-18 22:07:05 +0100

    Docblocks

--- App/Utility.php
+++ App/Utility.php
@@ -7,6 +7,14 @@ namespace App;
 class Utility
 {
 
+    /**
+     * Neatly prints an array
+     * 
+     * Outputs an array surrounded by pre tags for formatting
+     * 
+     * @param array $array The array to output
+     */
+
 
     //static method, non dobbiamo istanziare una classe per accederci. classe utility solitamente e' fatta da static method per metodi di servizio generale
     public static function printArr(array $array)
